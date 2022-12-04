911e9a0e94f6823f5318720dea8c1825f59adad6
bivanestor <bivanestor@tutanota.com>
2022-12-04 19:43:57 +0200

    Add Game module

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 const Player = (sign, name) => {
 	return { sign, name };
 };
+
+const Game = (() => {
+	let board = [];
+	const checkForEnd = () => {
+		console.log("I'm not implemented... yet");
+	};
+	return { board, checkForEnd };
+})();
