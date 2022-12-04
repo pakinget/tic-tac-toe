3ae093c35d1fee2e06886f30d3ea7d8dd6423fe4
bivanestor <bivanestor@tutanota.com>
2022-12-04 20:37:10 +0200

    Add display function to Game module

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const Player = (sign, name) => {
 };
 
 const Gameboard = (() => {
-	let board = [];
+	let board = [["x", "o", "x"], ["x", "o", "x"], ["o", "x", "o"]];
 	return { board };
 })();
 
@@ -13,5 +13,16 @@ const Game = (() => {
 	const start = (playerA, playerB) => {
 		Gameboard.board = [];
 	};
-	return { initiate };
+	const display = () => {
+		const boardContainer = document.createElement("div");
+		for (let i = 0; i < 9; i++) {
+			const cell = document.createElement("div");
+			cell.textContent = `${Gameboard.board[Math.floor(i / 3)][Math.floor(i - Math.floor(i / 3) * 3)]}`;
+			boardContainer.appendChild(cell);
+			console.log(cell);
+		}
+		const body = document.querySelector("body");
+		body.appendChild(boardContainer);
+	};
+	return { start, display };
 })();
