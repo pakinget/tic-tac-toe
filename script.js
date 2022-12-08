e20a9de98283a483e405ddd59ffafde38d83b6b0
bivanestor <bivanestor@tutanota.com>
2022-12-08 21:00:12 +0200

    Add Game.start function to display the Gameboard

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const Player = (sign, name, wins = 0) => {
+const Player = (sign, name, wins) => {
 	return { sign, name, wins };
 };
 
@@ -8,7 +8,7 @@ const Gameboard = (() => {
 	let board = [["x", "o", "x"], ["x", "o", "x"], ["o", "x", "o"]];
 	let node;
 	const reset = () => {
-		Gameboard.board = [];
+		Gameboard.board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]];
 		Gameboard.node = undefined;
 	};
 	return { board, node, reset };
@@ -17,11 +17,19 @@ const Gameboard = (() => {
 const Game = (() => {
 	const players = [];
 	const start = (playerX, playerO) => {
+		// Swapping the players is playerX's sign isn't actually X
+		if (playerX.sign !== "x") {
+			const temp = playerO;
+			playerO = playerX;
+			playerX = temp;
+		}
 		Gameboard.reset();
+		Game.display();
 	};
 	const display = () => {
 		const boardContainer = document.createElement("div");
 		boardContainer.classList.add("boardContainer");
+		Gameboard.node = boardContainer;
 		for (let i = 0; i < 9; i++) {
 			const cell = document.createElement("div");
 			cell.textContent = `${Gameboard.board[Math.floor(i / 3)][Math.floor(i - Math.floor(i / 3) * 3)]}`;
@@ -32,3 +40,7 @@ const Game = (() => {
 	};
 	return { players, start, display };
 })();
+
+let a = Player("a", "x", 0);
+let b = Player("b", "x", 0);
+Game.start(a, b);
