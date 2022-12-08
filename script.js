754cccdd48ff4555b5689048b4a4419deb9bd33d
bivanestor <bivanestor@tutanota.com>
2022-12-08 21:11:35 +0200

    Move Game.display() to Gameboard.create()

--- script.js
+++ script.js
@@ -7,11 +7,26 @@ const Player = (sign, name, wins) => {
 const Gameboard = (() => {
 	let board = [["x", "o", "x"], ["x", "o", "x"], ["o", "x", "o"]];
 	let node;
+	const create = () => {
+		const boardContainer = document.createElement("div");
+		boardContainer.classList.add("boardContainer");
+		Gameboard.node = boardContainer;
+		for (let i = 0; i < 9; i++) {
+			const cell = document.createElement("div");
+			cell.textContent = `${Gameboard.board[Math.floor(i / 3)][Math.floor(i - Math.floor(i / 3) * 3)]}`;
+			/* cell.addEventListener("click", () => {
+
+			}); */
+			boardContainer.appendChild(cell);
+		}
+		const body = document.querySelector("body");
+		body.appendChild(boardContainer);
+	};
 	const reset = () => {
 		Gameboard.board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]];
 		Gameboard.node = undefined;
 	};
-	return { board, node, reset };
+	return { board, node, rese, create };
 })();
 
 const Game = (() => {
@@ -26,19 +41,7 @@ const Game = (() => {
 		Gameboard.reset();
 		Game.display();
 	};
-	const display = () => {
-		const boardContainer = document.createElement("div");
-		boardContainer.classList.add("boardContainer");
-		Gameboard.node = boardContainer;
-		for (let i = 0; i < 9; i++) {
-			const cell = document.createElement("div");
-			cell.textContent = `${Gameboard.board[Math.floor(i / 3)][Math.floor(i - Math.floor(i / 3) * 3)]}`;
-			boardContainer.appendChild(cell);
-		}
-		const body = document.querySelector("body");
-		body.appendChild(boardContainer);
-	};
-	return { players, start, display };
+	return { players, start };
 })();
 
 let a = Player("a", "x", 0);
